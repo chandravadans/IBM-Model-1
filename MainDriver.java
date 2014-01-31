08170a99858292ead5e49792f2f39dd1bdb10622
Chandravadan S <s.chandravadan@gmail.com>
2014-01-31 12:59:51 +0530

    Refined Driver class

--- MainDriver.java
+++ MainDriver.java
@@ -15,10 +15,18 @@ public class MainDriver {
 		englishFile=in.next();
 		
 		System.out.println("Training model.. ");
+		long st=System.currentTimeMillis();
 		String args1[]=new String[2];
 		args1[0]=englishFile;
 		args1[1]=foreignFile;
 		EMAlgorithm.train(args1);
+		System.out.println("************Time for training: "+(System.currentTimeMillis()-st)/1000+" s***********");
+		
+		System.out.println("Step 2: Decoding ");
+		System.out.println("Enter the name of the file containing foreign sentences to be translated");
+		String translationFile=in.next();
+		
+		in.close();
 		
 		
 		
